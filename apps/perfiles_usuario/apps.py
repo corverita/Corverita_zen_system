666069d1aa8f3ad15787f5f69aafa62a6db211b4
corverita <oscar.corvera@live.com>
2024-08-14 15:28:52 -0600

    Cambio el nombre de las apps para que sean reconocidas por las configuraciones del proyecto

--- apps/perfiles_usuario/apps.py
+++ apps/perfiles_usuario/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PerfilesUsuarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'perfiles_usuario'
+    name = 'apps.perfiles_usuario'
